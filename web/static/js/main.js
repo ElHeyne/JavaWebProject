f9b5d7e950005e049e7971e665a358a6aa4d192d
HY_ElCositas <erikvvh06@gmail.com>
2025-04-08 17:03:01 +0200

    Javascript Cleanup

--- web/static/js/main.js
+++ web/static/js/main.js
@@ -1,26 +1,26 @@
-function send(){
-    var ehttp = new XMLHttpRequest();
+function send() {
+    let shttp = new XMLHttpRequest();
 
-    ehttp.onreadystatechange = function () {
+    shttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("name").value = "";  
+            document.getElementById("name").value = "";
         }
     };
 
-    ehttp.open("POST", "http://localhost:8080/JavaWebProject/Index", true);
-    ehttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    ehttp.send("name="+document.getElementById("name").value);
+    shttp.open("POST", "http://localhost:8080/JavaWebProject/Index", true);
+    shttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    shttp.send("name=" + document.getElementById("name").value);
 }
 
-function get(){
-    var http = new XMLHttpRequest();
+function get() {
+    let ghttp = new XMLHttpRequest();
 
-    http.open("GET", "http://localhost:8080/JavaWebProject/Index", true);
-    http.send();
-
-    http.onreadystatechange = function () {
-        if (http.readyState == 4 && http.status == 200) {
-            document.getElementById("content").innerHTML = http.responseText;
+    ghttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = ghttp.responseText;
         }
-    }
+    };
+
+    ghttp.open("GET", "http://localhost:8080/JavaWebProject/Index", true);
+    ghttp.send();
 }
